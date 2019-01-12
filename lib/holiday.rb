90ef11d132925fd0dfa2a766cb2beea3084eb612
A Glazer <avigayilglazer@gmail.com>
2019-01-12 20:00:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,9 +67,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-holiday_supplies.each {|season, holiday| puts season}
+holiday_supplies.each {|season, holiday| puts "#{season}}
 
-    
+
   holiday.collect do |holiday, item|
 
     puts "#{season}:"
