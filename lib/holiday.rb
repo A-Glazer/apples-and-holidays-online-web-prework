b118b03323a90081358e365b17eb354b956be55e
A Glazer <avigayilglazer@gmail.com>
2019-01-12 20:08:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
 
 holiday_supplies.collect do |season, holiday|
   new_season = season.to_s.capitalize!
-  puts new_season + ":"
+  puts new_season << ":"
 end
 
 
